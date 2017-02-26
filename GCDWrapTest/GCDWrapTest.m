2e51c158b078ada72c3494bd088d47425b1da245
Silence <374619540@qq.com>
2017-02-26 22:50:25 +0800

    Update Test

--- GCDWrapTest/GCDWrapTest.m
+++ GCDWrapTest/GCDWrapTest.m
@@ -7,6 +7,8 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <libkern/OSAtomic.h>
+#import "GCDWrap.h"
 
 @interface GCDWrapTest : XCTestCase
 
@@ -14,26 +16,44 @@
 
 @implementation GCDWrapTest
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+- (void)testMainQueue{
+    XCTAssertEqual([SIQueue mainQueue].metaQueue, dispatch_get_main_queue()) ;
+    
+    XCTAssertTrue([SIQueue isMainQueue]) ;
+    [[SIQueue globalQueue] syncBlock:^{
+        XCTAssertFalse([SIQueue isMainQueue]) ;
+    }] ;
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+- (void)testGlobalQueue{
+    XCTAssertEqual([SIQueue globalQueue].metaQueue, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0));
+    XCTAssertEqual([SIQueue highPriorityGlobalQueue].metaQueue, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0));
+    XCTAssertEqual([SIQueue lowPriorityGlobalQueue].metaQueue, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0));
+    XCTAssertEqual([SIQueue backgroundPriorityGlobalQueue].metaQueue, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0));
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testQueueBlockInGroup {
+    SIQueue *queue = [SIQueue concurrentQueue] ;
+    SIGroup *group = [[SIGroup alloc]init] ;
+    
+    for (int i = 0; i < 100; ++i) {
+        [queue asyncBlock:^{ NSLog(@"%lf",[[NSDate date]timeIntervalSince1970]) ; } inGroup:group];
+    }
+    [queue notifyBlock:^{NSLog(@"Notify") ;} inGroup:group] ;
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testApplyBlockQueue{
+    SIQueue *queue = [SIQueue concurrentQueue] ;
+    [queue applyBlock:^(size_t i) {
+        NSLog(@"%zu",i) ;
+    } iterationCount:100] ;
+}
+
+- (void)testMacro{
+    GCDMain(^{
+            XCTAssertTrue([SIQueue isMainQueue]) ;
+        }
+    ) ;
 }
 
 @end
